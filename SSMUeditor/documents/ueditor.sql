59cf21187a37e9650a07eaaf7f3e50d60206c82a
GZzzhsmart <1729340612@qq.com>
2017-10-19 09:52:46 +0800

    删除

--- SSMUeditor/documents/ueditor.sql
+++
@@ -1,8 +0,0 @@
-drop table if EXISTS ueditor;
-create table ueditor(
-  id int primary key AUTO_INCREMENT,
-  title varchar(50) not null,
-  content TEXT ,
-  createTime DATETIME DEFAULT  now()
-
-)engine=innodb default charset=utf8;
